fc4afd123da836e65e95d9763465fcd39b2a82be
ruanzindeluxe <128759460+ruanzinn7b@users.noreply.github.com>
2023-06-02 21:28:16 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+body{
+background-image:linear-gradient(45deg,red,black);
+
+
+
+}
+</style>
+<body>
+    <center>
+    <h1>LOGIN</h1>
+    <form name= "login" action="logado.php" method= "POST">
+    Login: <input type= "text" name = "login" required><br>
+    Senha: <input type= "password" name = "senha" required><br><br>
+    <input type="submit" id="entrar" value="Entrar">
+     </form>
+     
+</center>
+</body>
+</html>
