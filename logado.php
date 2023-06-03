b5d08f4917902661f076bce11929c791809bb3e8
ruanzindeluxe <128759460+ruanzinn7b@users.noreply.github.com>
2023-06-02 21:27:51 -0300

    Create logado.php

---
+++ logado.php
@@ -0,0 +1,24 @@
+<?php
+include('conexao.php');
+
+$login = isset($_POST['login'])? $_POST['login']:'';
+$senha = isset($_POST['senha'])? $_POST['senha']:'';
+
+$select = "select nome,login,senha from login
+where login = '$login' and senha = '$senha'";
+
+$execute = mysqli_query($conexao,$select);
+
+$dados = mysqli_fetch_row($execute);
+
+if($login== $dados[1] && $senha== $dados[2]){
+    header('location: index.php');
+}else{
+    header('location:login.php');
+}
+?>
+<html>
+<body>
+<center><h1>LOGADO</h1></center>  
+</body>
+</html>
