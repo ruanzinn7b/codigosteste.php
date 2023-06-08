b1c0b991af350a70daa38de1920da927a1cad57e
ruanzindeluxe <128759460+ruanzinn7b@users.noreply.github.com>
2023-06-07 21:33:25 -0300

    Create cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,14 @@
+<html>
+;
+<body>
+    <center>
+ 
+<form id= "cadastro" action= "cadastro.php" method= "POST">
+    nome: <input type = "text" name="nome" required> <br>
+    login: <input type = "text" name="login" required><br>
+    senha: <input type = "password"name="senha"required><br><br>
+    <input type= "submit" id= "" value="cadastrar">
+  </form>
+</center>
+</body>
+</html>
